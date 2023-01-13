fe613a3bf9b56e1cd3f195e2f4339f957d483f02
WiDayn <widayn@prmlk.com>
2023-01-13 10:51:10 +0800

    feat: load default themes

--- internal/controller/homeController.go
+++ internal/controller/homeController.go
@@ -6,10 +6,18 @@ import (
 )
 
 func homeController(router *gin.Engine) {
-	router.LoadHTMLGlob("./themes/frontstage/plain-sekai/home/*")
+	// 暂时的解决方法
+	router.LoadHTMLFiles(
+		"./internal/chunkLoader/chunk.tmpl",
+		"./themes/frontStage/plain-sekai/pages/home/content.tmpl",
+		"./themes/frontStage/plain-sekai/pages/post/content.tmpl",
+		"./themes/frontStage/plain-sekai/template/footer/footer.tmpl",
+		"./themes/frontStage/plain-sekai/template/header/header.tmpl",
+		"./themes/frontStage/plain-sekai/template/mask/mask.tmpl",
+	)
 	router.GET("/", func(c *gin.Context) {
-		c.HTML(http.StatusOK, "home.tmpl", gin.H{
-			"title": "Main website",
+		c.HTML(http.StatusOK, "chunk", gin.H{
+			"sekaiPageTitle": "Main website",
 		})
 	})
 }
