f01d8280cdcdea842c0a4494db667784cea451cb
DatGuy <mcfun001@126.com>
2023-01-16 00:20:48 +0800

    fix: move login page to backstage folder.

--- internal/controller/staticController.go
+++ internal/controller/staticController.go
@@ -4,4 +4,5 @@ import "github.com/gin-gonic/gin"
 
 func staticController(router *gin.Engine) {
 	router.StaticFile("/stylesheets/styles.css", "./themes/frontStage/plain-sekai/template/header/stylesheets/styles.css")
+	router.StaticFile("/backstage/user/stylesheets/styles.css", "./themes/backStage/plain-sekai/template/header/stylesheets/styles.css")
 }
