673c409d2f4ff2d895e5bb1e7e546397f6a1471e
WiDayn <widayn@prmlk.com>
2023-01-15 22:53:16 +0800

    feat: sekaiSiteRoot

--- internal/controller/homeController.go
+++ internal/controller/homeController.go
@@ -9,6 +9,7 @@ func homeController(router *gin.Engine) {
 	router.GET("/", func(c *gin.Context) {
 		c.HTML(http.StatusOK, "chunk.tmpl", gin.H{
 			"sekaiPageTitle": "Home",
+			"sekaiSiteRoot":  "http://localhost:12070",
 		})
 	})
 }
