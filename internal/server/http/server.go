8f1942da754cfdea68cbce8b45160a4b034dcc0d
WiDayn <widayn@prmlk.com>
2023-01-15 18:01:07 +0800

    fix: http server shutdown

--- internal/server/http/server.go
+++ internal/server/http/server.go
@@ -6,8 +6,14 @@ import (
 	"SeKai/internal/controller"
 	"SeKai/internal/logger"
 	"SeKai/internal/middleware"
+	"context"
 	"github.com/gin-gonic/gin"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
 	"strconv"
+	"time"
 )
 
 func StartHTTP() {
@@ -20,7 +26,26 @@ func StartHTTP() {
 	// 加载控制器
 	controller.InitController(router)
 
-	if err := router.Run(":" + strconv.Itoa(config.ApplicationConfig.HTTP.Port)); err != nil {
-		logger.ServerLogger.Panic(err)
+	srv := &http.Server{
+		Addr:    ":" + strconv.Itoa(config.ApplicationConfig.HTTP.Port),
+		Handler: router,
 	}
+
+	go func() {
+		if err := srv.ListenAndServe(); err != nil {
+			logger.ServerLogger.Panic("HTTP服务器开启失败：" + err.Error())
+		}
+	}()
+
+	quit := make(chan os.Signal)
+	signal.Notify(quit, os.Interrupt)
+	<-quit
+	logger.ServerLogger.Info("Shutdown Server ...")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	if err := srv.Shutdown(ctx); err != nil {
+		log.Fatal("Server Shutdown:", err)
+	}
+	log.Println("Server exiting")
 }
