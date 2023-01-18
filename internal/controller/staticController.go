e88feb3191c1bdb2ea4bb2cdeab26809458e4379
WiDayn <widayn@prmlk.com>
2023-01-18 16:23:35 +0800

    fix: unify staticMap and EntrancesMap format

--- internal/controller/staticController.go
+++ internal/controller/staticController.go
@@ -7,11 +7,9 @@ import (
 
 func staticController(router *gin.Engine) {
 	for _, staticFile := range themeLoader.BackStageTheme.StaticFiles {
-		staticFile.ControllerURL = "/" + staticFile.ControllerURL
 		router.StaticFile(staticFile.ControllerURL, staticFile.FileDir)
 	}
 	for _, staticFile := range themeLoader.FrontStageTheme.StaticFiles {
-		staticFile.ControllerURL = "/" + staticFile.ControllerURL
 		router.StaticFile(staticFile.ControllerURL, staticFile.FileDir)
 	}
 }
