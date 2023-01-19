668372e1afad693e9f31e704dbf159b76d628f57
DatGuy <mcfun001@126.com>
2023-01-20 00:59:29 +0800

    refactor: modularized theme.

---
+++ themes/backStage/plain-sekai/template/header/scripts/function.js
@@ -0,0 +1,25 @@
+//     original $rgba-grey-2 rgba(227,227,227,1)
+//     hover $rgba-grey-3 rgba(126,126,126,1)
+//     focus $rgba-info-0 rgba(  7,187,195,1)
+
+// TODO: debug cookie funtions
+function setCookie(key, value, expires) {
+    var oDate = new Date();
+    oDate.setDate(oDate.getDate() + expires);
+    document.cookie = key + '=' + value + ';expires=' + oDate;
+}
+
+function removeCookie(key) {
+    setCookie(key, '', -1);
+}
+
+function getCookie(key) {
+    var cookieArr = document.cookie.split('; ');
+    for (var i = 0; i < cookieArr.length; i++) {
+        var arr = cookieArr[i].split('=');
+        if (arr[0] === key) {
+            return arr[1];
+        }
+    }
+    return false;
+}
---
+++ themes/frontStage/plain-sekai/template/header/scripts/styles.js
@@ -0,0 +1,40 @@
+$(".sekai-input-warp").hover(function handlerIn() {
+    let label = $(this).children(".sekai-input-label");
+    if (label.length < 0) {
+        return false;
+    } else {
+        label.css("color", "rgba(126,126,126,1)");
+    }
+}, function handlerOut() {
+    let label = $(this).children(".sekai-input-label");
+    if (label.length < 0) {
+        console.log(label.attr("focus") === "true");
+        return false;
+    } else {
+        if (label.attr("focus") !== "true") {
+            label.css("color", "rgba(171,171,171,1)");
+        }
+    }
+});
+$(".sekai-input").focus(function () {
+    let label = $(this).parent().children(".sekai-input-label");
+    console.log(label.content);
+    if (label.length < 0) {
+        return false;
+    } else {
+        label.css("color", "rgba(  7,187,195,1)");
+        label.attr("focus", true);
+    }
+});
+$(".sekai-input").focusout(function () {
+    let label = $(this).parent().children(".sekai-input-label");
+    if (label.length < 0) {
+        return false;
+    } else {
+        label.css("color", "rgba(171,171,171,1)");
+        label.attr("focus", false);
+    }
+});
+$.onload(function () {
+    $("html").addClass("noscroll");
+});
